50867bd9c7c446eb46ff2c2b6ead5c3a0e8a87d1
Eric Wu <ericwu692@yahoo.com>
2021-06-24 12:07:16 -0400

    added movie data

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 
-const Movie = () => {
+const IMG_API = "https://image.tmdb.org/t/p/w1280";
+
+const Movie = ({ title, poster_path, overview, vote_average }) => {
     return (
-        <div>
-            This is a movie
+        <div className="movie">
+
+
+            <div>
+                <img src={IMG_API + poster_path} alte={title} />
+            </div>
+            <h3>{title}</h3>
+            <span> {vote_average} </span>
         </div>
     )
 
