79470f07e845d5de216b280b4e449fb9f1a8b795
Eric Wu <ericwu692@yahoo.com>
2021-06-24 19:26:43 -0400

    added individual movie details

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,43 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
 import Movie from './components/Movie';
+import MenuIcon from '@material-ui/icons/Menu';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+import Button from '@material-ui/core/Button';
+import YouTubeIcon from '@material-ui/icons/YouTube';
+import { makeStyles } from "@material-ui/core/styles";
 
 const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=" + process.env.REACT_APP_MOVIE_API_KEY;
 const Search_API = "https://api.themoviedb.org/3/search/movie?&api_key=" + process.env.REACT_APP_MOVIE_API_KEY + "&query=";
+const IMG_API = "https://image.tmdb.org/t/p/w500";
+
+const useStyles = makeStyles((theme) => ({
+    modal: {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    paper: {
+        width: "90%",
+        height: "80%",
+        backgroundColor: "#39445a",
+        border: "1px solid #282c34",
+        borderRadius: 10,
+        color: "white",
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(1, 1, 3),
+    },
+}));
 
 function App() {
+    const classes = useStyles();
     const [movies, setMovies] = useState([]);
     const [searchTerm, setSearchTerm] = useState('');
+    const [modalOpen, setModalOpen] = useState(false);
+    const [selectedMovie, setSelectedMovie] = useState({});
+    const [video, setVideo] = useState({});
 
     useEffect(() => {
         if (window.location.href.includes('search?=')) {
@@ -19,7 +49,8 @@ function App() {
     }, [])
 
     const getMovies = (API) => {
-        fetch(API).then(res => res.json())
+        fetch(API)
+            .then(res => res.json())
             .then(data => {
                 setMovies(data.results);
             });
@@ -35,9 +66,28 @@ function App() {
     const handleOnChange = (e) => {
         setSearchTerm(e.target.value);
     }
+
+    const handleCloseModal = () => {
+        setModalOpen(false);
+    }
+
+    const handleClick = (movie) => {
+        setSelectedMovie(movie);
+        fetch(`https://api.themoviedb.org/3/movie/${movie.id}/videos?api_key=${process.env.REACT_APP_MOVIE_API_KEY}&language=en-US`)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data.results);
+                setVideo(data.results);
+            })
+        setModalOpen(true);
+    }
     return (
         <>
             <header>
+                <MenuIcon />
+                <span className="title">
+                    🎬 MovieDB 🎥
+                </span>
                 <form onSubmit={handleOnSubmit}>
                     <input
                         className="search"
@@ -47,13 +97,95 @@ function App() {
                         onChange={handleOnChange}
                     />
                 </form>
+
             </header>
             < div className="movie-container">
 
                 {movies.length > 0 && movies.map(movie => {
-                    return <Movie key={movie.id} {...movie} />
+                    return (
+                        <button
+                            type="button"
+                            key={movie.id}
+                            className={"button"}
+                            onClick={() => handleClick(movie)}
+                        >
+                            <Movie  {...movie} />
+                        </button>
+                    )
                 })}
             </div>
+
+            <Modal
+                aria-labelledby="transition-modal-title"
+                aria-describedby="transition-modal-description"
+                className={classes.modal}
+                open={modalOpen}
+                onClose={handleCloseModal}
+                closeAfterTransition
+                BackdropComponent={Backdrop}
+                BackdropProps={{
+                    timeout: 500,
+                }}
+            >
+                <Fade in={modalOpen}>
+                    {selectedMovie && (
+                        <div className={classes.paper}>
+                            <div className="ContentModal">
+                                <img
+                                    src={
+                                        selectedMovie.poster_path
+                                            ? IMG_API + selectedMovie.poster_path
+                                            : "https://www.movienewz.com/img/films/poster-holder.jpg"
+                                    }
+                                    alt={selectedMovie.name || selectedMovie.title}
+                                    className="ContentModal__portrait"
+                                />
+                                <img
+                                    src={
+                                        selectedMovie.backdrop_path
+                                            ? IMG_API + selectedMovie.backdrop_path
+                                            : "https://user-images.githubusercontent.com/10515204/56117400-9a911800-5f85-11e9-878b-3f998609a6c8.jpg"
+                                    }
+                                    alt={selectedMovie.name || selectedMovie.title}
+                                    className="ContentModal__landscape"
+                                />
+                                <div className="ContentModal__about">
+                                    <span className="ContentModal__title">
+                                        {selectedMovie.name || selectedMovie.title} (
+                                        {(
+                                            selectedMovie.first_air_date ||
+                                            selectedMovie.release_date ||
+                                            "-----"
+                                        ).substring(0, 4)}
+                                        )
+                                    </span>
+                                    {selectedMovie.tagline && (
+                                        <i className="tagline">{selectedMovie.tagline}</i>
+                                    )}
+
+                                    <span className="ContentModal__description">
+                                        {selectedMovie.overview}
+                                    </span>
+
+                                    {/* <div>
+                                        <Carousel id={id} media_type={media_type} />
+                                    </div> */}
+
+                                    <Button
+                                        variant="contained"
+                                        startIcon={<YouTubeIcon />}
+                                        color="secondary"
+                                        target="__blank"
+                                        href={`https://www.youtube.com/watch?v=${video}`}
+                                    >
+                                        Watch the Trailer
+                                    </Button>
+                                </div>
+                            </div>
+                        </div>
+                    )}
+                </Fade>
+            </Modal>
         </>
     );
 }
--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -11,10 +11,10 @@ const setVoteClass = (vote) => {
         return 'red';
     }
 }
-const Movie = ({ title, poster_path, overview, vote_average }) => {
+const Movie = ({ title, poster_path, overview, vote_average}) => {
     return (
         <div className="movie">
-            <img src={poster_path ? IMG_API + poster_path : 'https://images.unsplash.com/photo-1542204165-65bf26472b9b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80'} alte={title} />
+            <img src={poster_path ? IMG_API + poster_path : 'https://images.unsplash.com/photo-1542204165-65bf26472b9b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80'} alt={title} />
             <div className="movie-info">
                 <h3>{title}</h3>
                 <span
