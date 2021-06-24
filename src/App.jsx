6372890587087dae8c9f2bd811f06ed63c4aa828
Eric Wu <ericwu692@yahoo.com>
2021-06-24 12:01:11 -0400

    added api call

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
 import './App.css';
+import React, { useEffect, useState } from 'react';
 import Movie from './components/Movie';
 
+const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=" + process.env.REACT_APP_MOVIE_API_KEY;
+const IMG_API = "https://image.tmdb.org/t/p/w1280";
+const Search_API = "https://api.themoviedb.org/3/search/movie?&api_key=" + process.env.REACT_APP_MOVIE_API_KEY + "&query=";
+
 function App() {
-    const movies = ['1', '2', '3'];
-    const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=" + process.env.REACT_APP_MOVIE_API_KEY;
-    const IMG_API = "https://image.tmdb.org/t/p/w1280";
-    const Search_API = "https://api.themoviedb.org/3/search/movie?&api_key=" + process.env.REACT_APP_MOVIE_API_KEY +"&query=";
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        fetch(FEATURED_API).then( res => res.json())
+        .then(data => {
+            console.log(data);
+            setMovies(data.results);
+        });
+    }, [])
+
     return (
 
         < div >
-            {movies.map(movie => {
+            {movies.length > 0 && movies.map(movie => {
                 return <Movie />
             })}
         </div>
