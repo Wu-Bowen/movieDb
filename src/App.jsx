a5fa4d8d51b7c8286c5f34d365e0449c84079faa
Eric Wu <ericwu692@yahoo.com>
2021-06-24 11:40:41 -0400

    added apis

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import Movie from './components/Movie';
 
 function App() {
     const movies = ['1', '2', '3'];
-    const API_KEY = process.env.REACT_APP_MOVIE_API_KEY;
+    const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=" + process.env.REACT_APP_MOVIE_API_KEY;
+    const IMG_API = "https://image.tmdb.org/t/p/w1280";
+    const Search_API = "https://api.themoviedb.org/3/search/movie?&api_key=" + process.env.REACT_APP_MOVIE_API_KEY +"&query=";
     return (
 
         < div >
-            {API_KEY}
             {movies.map(movie => {
                 return <Movie />
             })}
