09e5b67cee1e75b59d09104ed863dee4765f2b24
Eric Wu <ericwu692@yahoo.com>
2021-06-24 11:29:41 -0400

    is it working?

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import Movie from './components/Movie';
 
 function App() {
     const movies = ['1', '2', '3'];
+    const API_KEY = process.env.REACT_APP_MOVIE_API_KEY;
     return (
 
         < div >
+            {API_KEY}
             {movies.map(movie => {
                 return <Movie />
             })}
