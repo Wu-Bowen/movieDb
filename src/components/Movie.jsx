29f97cd302e0e8b2981ed47f9451f65d4cd4dca0
Eric Wu <ericwu692@yahoo.com>
2021-06-24 12:25:19 -0400

    added movie desc

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -13,6 +13,10 @@ const Movie = ({ title, poster_path, overview, vote_average }) => {
                 <h3>{title}</h3>
                 <span> {vote_average} </span>
             </div>
+            <div className="movie-hover">
+                <h2> Summary: </h2>
+                <p> {overview} </p>
+            </div>
         </div>
     )
 
